36763c8fc7b6ae1fc04689eac4a1dee8d6a2baf7
albezong <albertobeto263zongua@gmail.com>
2025-08-08 11:32:47 -0600

    Se agregan cambios

---
+++ apiMIRAI_Construcciones/AppQrLector.UnitWork/Contract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppQrLector.UnitWork.Contract
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<Usuario> Usuarios { get; }
+        //IRepository<Usuario> Usuarios { get; }
+        int Complete();
+        bool GuardarCambios(int id, byte[] QR);
+    }
+}
---
+++ apiMIRAI_Construcciones/AppQrLector.UnitWork/Helper/HCrearImagen/CrearImagen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AppQrLector.UnitWork.Helper.HCrearImagen
+{
+    public class CrearImagen
+    {
+    }
+
+    public class ImagenDesdeSQL
+    {
+        public static void MostrarImagen(string connectionString, int idImagen, PictureBox pictureBox)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM QrEquipos WHERE id = @id"; // Reemplaza 'tuTabla' y 'id'
+                    //SELECT* FROM QrEquipos WHERE idQrEquipos = 1;
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", idImagen);
+                        byte[] imageData = (byte[])command.ExecuteScalar();
+
+                        if (imageData != null)
+                        {
+                            using (MemoryStream ms = new MemoryStream(imageData))
+                            {
+                                pictureBox.Image = Image.FromStream(ms);
+                            }
+                        }
+                        else
+                        {
+                            // Manejar el caso donde no se encuentra la imagen
+                            //MessageBox.Silent("Imagen no encontrada.");
+                            //pictureBox.Image = null; // Limpiar el PictureBox si no hay imagen
+                            var Xm = 0;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores de la base de datos o conversión
+                MessageBox.Show($"Error al cargar la imagen: {ex.Message}");
+            }
+        }
+    }
+}
